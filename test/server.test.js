b82fb91261454cd903e9cb5b7e91313d2fa94587
Aleksandr Ignatyev <vigntom.2708@gmail.com>
2017-08-01 11:27:04 +0300

    Add routes

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,34 @@
+import request from 'supertest'
 import test from 'ava'
 import server from './../lib/server'
 
 test(t => {
   t.true(true)
+})
+
+function requestAndTest (title, route, task) {
+  test.cb(title, t => {
+    request(server)
+      .get(route)
+      .expect('Content-Type', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        t.ifError(err, "Errors aren't allowed")
+        task(t, res)
+        t.end()
+      })
+  })
+}
+
+requestAndTest('Empty Request', '/', (t, res) => {
+  t.truthy(res.body.warning, 'Wrong request!')
+  t.regex(res.body.use, /^Use:\s.*/, 'Help message starts with Use:')
+})
+
+requestAndTest('Search Request', '/api?q=search+query', (t, res) => {
+  t.true(Array.isArray(res.body))
+})
+
+requestAndTest('Show latest search queries', '/latest', (t, res) => {
+  t.true(Array.isArray(res.body))
 })
